01ae9f8275fa9a4bd8dd55ba35482f0c38675cee
Kamu <joaopaulogabarrao@gmail.com>
2024-05-03 21:24:37 -0300

    inial commit

--- App.tsx
+++ App.tsx
@@ -1,6 +1,28 @@
-import { StyleSheet, Text, View } from "react-native";
-import { Groups } from "./src/screens/Groups";
+import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { Loading } from "@components/Loading";
+import theme from "@theme/index";
+import { Groups } from "@screens/Groups";
 
 export default function App() {
-  return <Groups />;
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
+  );
 }
